27ccb78070c5f6668117661a54bd98506381a495
mauridevcde <mauridevcde@gmail.com>
2022-11-28 16:03:22 -0300

    conociendo redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { increment, decrement, incrementBy2 } from './store/slices/counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const { counter } = useSelector( state => state.counter )
+
+  let dispatch = useDispatch()
 
   return (
     <div className="App">
@@ -14,11 +19,17 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <p> count is {counter}</p>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <button onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementBy2(2))}>
+          Increment by 2
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
